6bc55c7287c796f73d5a2f6e69d032f04bc54eed
bsm100100 <bsm100100@gmail.com>
2025-04-22 00:15:42 +0900

    new

---
+++ src/pages/todo/ModifyPage.tsx
@@ -0,0 +1,13 @@
+import { Params } from "react-router-dom";
+
+const ModifyPage = ({tno} : Params) => {
+
+    return (
+    <div className="text-3xl font-extrabold"> Todo Modify Page
+    </div>
+    );
+    
+    }
+    
+    export default ModifyPage;
+    
